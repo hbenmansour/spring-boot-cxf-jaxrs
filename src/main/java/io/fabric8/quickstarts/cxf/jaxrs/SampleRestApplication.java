c727e11ae7bdad647b5db0a94443c6555c2451fd
Mohamed Hamza Ben Mansour <mbenmans@redhat.com>
2018-04-17 15:41:18 +0200

    chnage tracer init

--- src/main/java/io/fabric8/quickstarts/cxf/jaxrs/SampleRestApplication.java
+++ src/main/java/io/fabric8/quickstarts/cxf/jaxrs/SampleRestApplication.java
@@ -26,8 +26,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import com.uber.jaeger.Configuration;
-import com.uber.jaeger.samplers.ProbabilisticSampler;
+import com.uber.jaeger.Tracer;
+
 
 @SpringBootApplication
 public class SampleRestApplication {
@@ -52,8 +52,7 @@ public class SampleRestApplication {
 
 	@Bean
 	public io.opentracing.Tracer jaegerTracer() {
-		return new Configuration("spring-boot", new Configuration.SamplerConfiguration(ProbabilisticSampler.TYPE, 1),
-				new Configuration.ReporterConfiguration()).getTracer();
+		return new Tracer.Builder("spring-boot").build();
 
 	}
 
