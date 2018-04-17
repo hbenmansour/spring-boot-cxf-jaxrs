cc19018c5363fcf8098ef0e12feff66358331a17
Mohamed Hamza Ben Mansour <mbenmans@redhat.com>
2018-04-17 15:15:24 +0200

    add explicit tracer configuration

--- src/main/java/io/fabric8/quickstarts/cxf/jaxrs/SampleRestApplication.java
+++ src/main/java/io/fabric8/quickstarts/cxf/jaxrs/SampleRestApplication.java
@@ -26,25 +26,35 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.uber.jaeger.Configuration;
+import com.uber.jaeger.samplers.ProbabilisticSampler;
+
 @SpringBootApplication
 public class SampleRestApplication {
 
-    @Autowired
-    private Bus bus;
+	@Autowired
+	private Bus bus;
 
-    public static void main(String[] args) {
-        SpringApplication.run(SampleRestApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SampleRestApplication.class, args);
+	}
 
-    @Bean
-    public Server rsServer() {
-        // setup CXF-RS
-        JAXRSServerFactoryBean endpoint = new JAXRSServerFactoryBean();
-        endpoint.setBus(bus);
-        endpoint.setServiceBeans(Arrays.<Object>asList(new HelloServiceImpl()));
-        endpoint.setAddress("/");
-        endpoint.setFeatures(Arrays.asList(new Swagger2Feature()));
-        return endpoint.create();
-    }
+	@Bean
+	public Server rsServer() {
+		// setup CXF-RS
+		JAXRSServerFactoryBean endpoint = new JAXRSServerFactoryBean();
+		endpoint.setBus(bus);
+		endpoint.setServiceBeans(Arrays.<Object>asList(new HelloServiceImpl()));
+		endpoint.setAddress("/");
+		endpoint.setFeatures(Arrays.asList(new Swagger2Feature()));
+		return endpoint.create();
+	}
+
+	@Bean
+	public io.opentracing.Tracer jaegerTracer() {
+		return new Configuration("spring-boot", new Configuration.SamplerConfiguration(ProbabilisticSampler.TYPE, 1),
+				new Configuration.ReporterConfiguration()).getTracer();
+
+	}
 
 }
